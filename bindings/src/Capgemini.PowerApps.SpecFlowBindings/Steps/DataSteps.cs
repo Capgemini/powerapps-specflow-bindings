1bef848d295a29164d98ca738d1e7394f0bfb828
Ewing, Max <max.ewing@capgemini.com>
2020-06-14 18:34:33 +0000

    Merged PR 582: Added steps to setup test data

---
+++ bindings/src/Capgemini.PowerApps.SpecFlowBindings/Steps/DataSteps.cs
@@ -0,0 +1,32 @@
+﻿namespace Capgemini.PowerApps.SpecFlowBindings.Steps
+{
+    using Capgemini.PowerApps.SpecFlowBindings;
+    using TechTalk.SpecFlow;
+
+    /// <summary>
+    /// Test setup step bindings for data.
+    /// </summary>
+    [Binding]
+    public class DataSteps : PowerAppsStepDefiner
+    {
+        /// <summary>
+        /// Opens a test record.
+        /// </summary>
+        /// <param name="alias">The alias of the test record.</param>
+        [Given(@"I have opened '(.*)'")]
+        public void GivenIHaveOpened(string alias)
+        {
+            TestDriver.OpenTestRecord(alias);
+        }
+
+        /// <summary>
+        /// Creates a test record.
+        /// </summary>
+        /// <param name="fileName">The name of the file containing the test record.</param>
+        [Given(@"I have created '(.*)'")]
+        public void GivenIHaveCreated(string fileName)
+        {
+            TestDriver.LoadTestData(TestDataRepository.GetTestData(fileName));
+        }
+    }
+}
