232f9a2834727169d67fdcddd8cdf342d7383713
Ewing, Max <max.ewing@capgemini.com>
2020-06-23 07:16:27 +0000

    Merged PR 586: Related grid bindings
    Added a several bindings for related grids:
    
    - Opening a related grid tab
    - Clicking on a related grid button
    - Asserting that a related grid flyout contains a button
    - Asserting that a related grid flyout does not contain a button
    
    Also added a utility steps bindings class:
    
    - Waiting for a number of seconds (to allow asynchronous tasks to process)
    
    Related work items: #4764

---
+++ bindings/src/Capgemini.PowerApps.SpecFlowBindings/Steps/RelatedGridSteps.cs
@@ -0,0 +1,66 @@
+﻿namespace Capgemini.PowerApps.SpecFlowBindings.Steps
+{
+    using Capgemini.PowerApps.SpecFlowBindings;
+    using FluentAssertions;
+    using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+    using OpenQA.Selenium;
+    using TechTalk.SpecFlow;
+
+    /// <summary>
+    /// Steps relating to related grids.
+    /// </summary>
+    [Binding]
+    public class RelatedGridSteps : PowerAppsStepDefiner
+    {
+        /// <summary>
+        /// Opens a tab under the 'Related' tab.
+        /// </summary>
+        /// <param name="relatedTabName">The name of the tab under the 'Related' tab.</param>
+        [When(@"I open the related '(.*)' tab")]
+        public static void WhenIOpenTheRelatedTab(string relatedTabName)
+        {
+            XrmApp.Entity.SelectTab("Related", relatedTabName);
+        }
+
+        /// <summary>
+        /// Clicks on a button in a related grid.
+        /// </summary>
+        /// <param name="buttonName">The name of the button.</param>
+        [When(@"I click the '(.*)' button on the related grid")]
+        public static void WhenIClickTheButtonOnTheRelatedGrid(string buttonName)
+        {
+            XrmApp.Entity.RelatedGrid.ClickCommand(buttonName);
+        }
+
+        /// <summary>
+        /// Asserts that a button is available in a button flyout on the related grid.
+        /// </summary>
+        /// <param name="button">The name of the button.</param>
+        [Then(@"I should see a '(.*)' button in the flyout on the related grid")]
+        public static void ThenIShouldSeeAButtonInTheFlyoutOnTheRelatedGrid(string button)
+        {
+            var overFlowContainer = Driver.FindElement(By.XPath(AppElements.Xpath[AppReference.Related.CommandBarOverflowContainer]));
+
+            overFlowContainer
+                .Invoking(container => container.FindElement(By.XPath(AppElements.Xpath[AppReference.Related.CommandBarSubButton].Replace("[NAME]", button))))
+                .Should()
+                .NotThrow(because: "the button in the flyout should exist");
+        }
+
+        /// <summary>
+        /// Asserts that a button is not available in a button flyout on the related grid.
+        /// </summary>
+        /// <param name="button">The name of the button.</param>
+        [Then(@"I should not see a '(.*)' button in the flyout on the related grid")]
+        public static void ThenIShouldNotSeeAButtonInTheFlyoutOnTheRelatedGrid(string button)
+        {
+            var overFlowContainer = Driver.FindElement(By.XPath(AppElements.Xpath[AppReference.Related.CommandBarOverflowContainer]));
+
+            overFlowContainer
+                .Invoking(container => container.FindElement(By.XPath(AppElements.Xpath[AppReference.Related.CommandBarSubButton].Replace("[NAME]", button))))
+                .Should()
+                .Throw<NoSuchElementException>(because: "the button in the flyout shouldn't exist");
+        }
+
+    }
+}
