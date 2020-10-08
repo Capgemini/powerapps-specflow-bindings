73af9f69c3d48119ba50804cfa070645561f8ff8
Max Ewing <max.ewing@capgemini.com>
2020-10-08 16:07:23 +0700

    Added logging to script

--- scripts/Remove-InvalidSymbolsPackageFiles.ps1
+++ scripts/Remove-InvalidSymbolsPackageFiles.ps1
@@ -7,13 +7,17 @@ param (
 Add-Type -assembly  System.IO.Compression.FileSystem
 
 Get-ChildItem "$SymbolPackageDirectory/*" -Include *.snupkg | ForEach-Object {
+    Write-Host "Removing invalid files from $($_.Name)"
     $oldName = [IO.Path]::GetFileName($_.Name)
     $newName = [IO.Path]::GetFileName([IO.Path]::ChangeExtension($_.Name, "zip"))
     $zipFile = Rename-Item -Path $_.FullName -NewName $newName -PassThru
     $zip = [System.IO.Compression.ZipFile]::Open($zipFile.FullName, [IO.Compression.ZipArchiveMode]::Update)
     
     $allowedExtensions = @('.pdb', '.nuspec', '.xml', '.psmdcp', '.rels', '.p7s')
-    ($zip.Entries | Where-Object { $allowedExtensions -notcontains [IO.Path]::GetExtension($_.Name) }) | ForEach-Object { $_.Delete() }
+    ($zip.Entries | Where-Object { $allowedExtensions -notcontains [IO.Path]::GetExtension($_.Name) }) | ForEach-Object { 
+        Write-Host "Removing $($_.Name)"
+        $_.Delete() 
+    }
     
     $zip.Dispose()
     
