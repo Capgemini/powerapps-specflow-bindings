69b7de78058fba2e61974f8b46e3d6f708d81a7c
Max Ewing <max.ewing@outlook.com>
2020-10-09 13:22:05 +0700

    feat: nuget package debugging with source link (#21)
    configured source link with github and updated pipeline to publish symbol package to nuget symbol server +semver: feature

---
+++ scripts/Remove-InvalidSymbolsPackageFiles.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $SymbolPackageDirectory
+)
+Add-Type -assembly  System.IO.Compression.FileSystem
+
+Get-ChildItem "$SymbolPackageDirectory/*" -Include *.snupkg | ForEach-Object {
+    Write-Host "Removing invalid files from $($_.Name)"
+    $oldName = [IO.Path]::GetFileName($_.Name)
+    $newName = [IO.Path]::GetFileName([IO.Path]::ChangeExtension($_.Name, "zip"))
+    $zipFile = Rename-Item -Path $_.FullName -NewName $newName -PassThru
+    $zip = [System.IO.Compression.ZipFile]::Open($zipFile.FullName, "Update")
+    
+    $allowedExtensions = @('.pdb', '.nuspec', '.xml', '.psmdcp', '.rels', '.p7s')
+    ($zip.Entries | Where-Object { $allowedExtensions -notcontains [IO.Path]::GetExtension($_.Name) }) | ForEach-Object { 
+        Write-Host "Removing $($_.Name)"
+        $_.Delete() 
+    }
+    
+    $zip.Dispose()
+    
+    Rename-Item -Path $zipFile.FullName -NewName $oldName
+}
