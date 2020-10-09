455573e2f3a65292a985aae2fa4166076383cc0a
Max Ewing <max.ewing@capgemini.com>
2020-10-09 13:10:09 +0700

    fix: ZipArchiveMode enum not found

--- scripts/Remove-InvalidSymbolsPackageFiles.ps1
+++ scripts/Remove-InvalidSymbolsPackageFiles.ps1
@@ -11,7 +11,7 @@ Get-ChildItem "$SymbolPackageDirectory/*" -Include *.snupkg | ForEach-Object {
     $oldName = [IO.Path]::GetFileName($_.Name)
     $newName = [IO.Path]::GetFileName([IO.Path]::ChangeExtension($_.Name, "zip"))
     $zipFile = Rename-Item -Path $_.FullName -NewName $newName -PassThru
-    $zip = [System.IO.Compression.ZipFile]::Open($zipFile.FullName, [IO.Compression.ZipArchiveMode]::Update)
+    $zip = [System.IO.Compression.ZipFile]::Open($zipFile.FullName, "Update")
     
     $allowedExtensions = @('.pdb', '.nuspec', '.xml', '.psmdcp', '.rels', '.p7s')
     ($zip.Entries | Where-Object { $allowedExtensions -notcontains [IO.Path]::GetExtension($_.Name) }) | ForEach-Object { 
