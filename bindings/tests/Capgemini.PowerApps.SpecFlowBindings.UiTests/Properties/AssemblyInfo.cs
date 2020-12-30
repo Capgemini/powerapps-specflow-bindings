deddc76f1d0cb451376e707d6fd60f5512bdf2d8
Max Ewing <max.ewing@capgemini.com>
2020-12-30 10:18:02 +0000

    test: set workers to use no. of cores

--- bindings/tests/Capgemini.PowerApps.SpecFlowBindings.UiTests/Properties/AssemblyInfo.cs
+++ bindings/tests/Capgemini.PowerApps.SpecFlowBindings.UiTests/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.ClassLevel)]
