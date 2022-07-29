165fc8f590e1d514acd37fc90100d4bfa2a89ac5
Max Ewing <max.ewing@outlook.com>
2022-07-29 22:01:36 +0100

    feat: add additional capabilities support
    Additional capabilities are often required to support the use of managed Selenium Grid services (e.g. account details are often passed this way).
    
    Consumers using their own Selenium Grids may also see some benefits to being able to pass these as well.

--- bindings/src/Capgemini.PowerApps.SpecFlowBindings/Configuration/BrowserOptionsWithProfileSupport.cs
+++ bindings/src/Capgemini.PowerApps.SpecFlowBindings/Configuration/BrowserOptionsWithProfileSupport.cs
@@ -1,20 +1,39 @@
 ﻿namespace Capgemini.PowerApps.SpecFlowBindings.Configuration
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using Capgemini.PowerApps.SpecFlowBindings.Extensions;
     using Microsoft.Dynamics365.UIAutomation.Browser;
+    using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Edge;
     using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
 
     /// <summary>
-    /// Extends the EasyRepro <see cref="BrowserOptions"/> class with additonal support for chrome profiles.
+    /// Extends the EasyRepro <see cref="BrowserOptions"/> class with support for additional configuration.
     /// </summary>
     public class BrowserOptionsWithProfileSupport : BrowserOptions, ICloneable
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="BrowserOptionsWithProfileSupport"/> class.
+        /// </summary>
+        public BrowserOptionsWithProfileSupport()
+            : base()
+        {
+            this.AdditionalCapabilities = new Dictionary<string, object>();
+        }
+
+        /// <summary>
         /// Gets or sets the directory to use as the user profile.
         /// </summary>
         public string ProfileDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the additional capabilities.
+        /// </summary>
+        public Dictionary<string, object> AdditionalCapabilities { get; set; }
 
         /// <inheritdoc/>
         public object Clone()
@@ -32,6 +51,8 @@
                 options.AddArgument($"--user-data-dir={this.ProfileDirectory}");
             }
 
+            this.AddAdditionalCapabilities(options);
+
             return options;
         }
 
@@ -46,7 +67,37 @@
                 options.AddArgument($"-profile \"{this.ProfileDirectory}\"");
             }
 
+            this.AddAdditionalCapabilities(options);
+
             return options;
+        }
+
+        /// <inheritdoc/>
+        public override EdgeOptions ToEdge()
+        {
+            var options = base.ToEdge();
+
+            this.AddAdditionalCapabilities(options);
+
+            return options;
+        }
+
+        /// <inheritdoc/>
+        public override InternetExplorerOptions ToInternetExplorer()
+        {
+            var options = base.ToInternetExplorer();
+
+            this.AddAdditionalCapabilities(options);
+
+            return options;
+        }
+
+        private void AddAdditionalCapabilities(DriverOptions options)
+        {
+            foreach (var desiredCapability in this.AdditionalCapabilities)
+            {
+                options.AddGlobalCapability(desiredCapability.Key, desiredCapability.Value);
+            }
         }
     }
 }
