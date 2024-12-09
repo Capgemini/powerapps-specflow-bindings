b254fdbcd57bf674237ee0e35a786365634f739e
Max Ewing <max.ewing@capgemini.com>
2024-12-09 15:07:54 +0000

    fix: invalid annotations in data
    Fixes #185

--- driver/src/repositories/currentUserRecordRepository.ts
+++ driver/src/repositories/currentUserRecordRepository.ts
@@ -1,15 +1,15 @@
-import RecordRepository from './recordRepository';
 import { Record } from '../data';
 import { AssociateRequest } from '../requests';
+import GenericRecordRepository from './genericRecordRepository';
 
 /**
- * Repository to handle CRUD operations for entities.
+ * Repository to handle CRUD operations for entities using the logged in user.
  *
  * @export
  * @class RecordRepository
  * @extends {Repository}
  */
-export default class CurrentUserRecordRepository implements RecordRepository {
+export default class CurrentUserRecordRepository extends GenericRecordRepository {
   private readonly webApi: Xrm.WebApiOnline;
 
   /**
@@ -17,24 +17,17 @@ export default class CurrentUserRecordRepository implements RecordRepository {
    * @param webApi The web API instance.
    */
   constructor(webApi: Xrm.WebApiOnline) {
+    super();
+
     this.webApi = webApi;
   }
 
-  /**
-   * Retrieves a record.
-   * @param logicalName The logical name of the record to retrieve.
-   * @param id The ID of the record to retrieve.
-   * @param query The query string.
-   */
+  /** @inheritdoc */
   public async retrieveRecord(logicalName: string, id: string, query?: string): Promise<any> {
     return this.webApi.retrieveRecord(logicalName, id, query);
   }
 
-  /**
-   * Retrieves multiple records.
-   * @param logicalName The logical name of the records to retrieve.
-   * @param query The query string.
-   */
+  /** @inheritdoc */
   public async retrieveMultipleRecords(
     logicalName: string,
     query: string,
@@ -42,28 +35,15 @@ export default class CurrentUserRecordRepository implements RecordRepository {
     return this.webApi.retrieveMultipleRecords(logicalName, query);
   }
 
-  /**
-     * Creates an entity record.
-     *
-     * @param {string} logicalName A logical name for the entity to create.
-     * @param {Record} record A record to create.
-     * @returns {Xrm.LookupValue} An entity reference to the created entity.
-     * @memberof RecordRepository
-     */
+  /** @inheritdoc */
   public async createRecord(logicalName: string, record: Record): Promise<Xrm.LookupValue> {
-    return this.webApi.createRecord(logicalName, record);
+    return this.webApi.createRecord(logicalName, this.sanitiseRecord(record));
   }
 
-  /**
-     * Upserts an entity record.
-     * @param {string} logicalName A logical name for the entity to upsert.
-     * @param {Record} record A record to upsert.
-     * @returns {Xrm.LookupValue} An entity reference to the upserted entity.
-     * @memberof RecordRepository
-     */
+  /** @inheritdoc */
   public async upsertRecord(logicalName: string, record: Record): Promise<Xrm.LookupValue> {
     if (!record['@key']) {
-      return this.webApi.createRecord(logicalName, record);
+      return this.webApi.createRecord(logicalName, this.sanitiseRecord(record));
     }
 
     const retrieveResponse = await this.webApi.retrieveMultipleRecords(
@@ -73,34 +53,20 @@ export default class CurrentUserRecordRepository implements RecordRepository {
 
     if (retrieveResponse.entities.length > 0) {
       const id = retrieveResponse.entities[0][`${logicalName}id`];
-      await this.webApi.updateRecord(logicalName, id, record);
+      await this.webApi.updateRecord(logicalName, id, this.sanitiseRecord(record));
 
       return { entityType: logicalName, id };
     }
 
-    return this.webApi.createRecord(logicalName, record);
+    return this.webApi.createRecord(logicalName, this.sanitiseRecord(record));
   }
 
-  /**
-     * Deletes an entity record.
-     *
-     * @param {Xrm.LookupValue} ref A reference to the entity to delete.
-     * @returns {Xrm.LookupValue} A reference to the deleted entity.
-     * @memberof RecordRepository
-     */
+  /** @inheritdoc */
   public async deleteRecord(ref: Xrm.LookupValue): Promise<Xrm.LookupValue> {
     return this.webApi.deleteRecord(ref.entityType, ref.id) as unknown as Xrm.LookupValue;
   }
 
-  /**
-     * Associates two records in a N:N Relationship.
-     *
-     * @param {Xrm.LookupValue} primaryRecord The Primary Record to associate.
-     * @param {Xrm.LookupValue[]} relatedRecords The Related Records to associate.
-     * @param {string} relationship The N:N Relationship Name.
-     * @returns {Xrm.ExecuteResponse} The Response from the execute request.
-     * @memberof RecordRepository
-     */
+  /** @inheritdoc */
   public async associateManyToManyRecords(
     primaryRecord: Xrm.LookupValue,
     relatedRecords: Xrm.LookupValue[],
